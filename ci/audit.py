15a9cce76722362e9ef1e7102a5ccccbb25d118f
Semjon Geist <semjon.geist@ionos.com>
2022-12-11 10:43:11 +0100

    try to fix missing msvc

--- ci/audit.py
+++ ci/audit.py
@@ -8,10 +8,11 @@ os.makedirs(target_dir, exist_ok=True)
 artifacts_file = os.listdir(source_dir)
 print(artifacts_file)
 user = os.environ.get("USER", "")
-pattern = f"(a0[+](sdist|build)[.][0-9].*[.]{user})"
 
 for file in artifacts_file:
     source = os.path.join(source_dir, file)
-    target = os.path.join(target_dir, re.sub(pattern, "", file.replace("linux", "manylinux")))
+    target = re.sub(f"^(.+)\\..+(-.+-.+-.+|.tar.gz)", "\\1\\2",
+                    re.sub(f"a0[+](sdist|build)[.][0-9]+", "",
+                           file.replace("linux", "manylinux")))
     print(f"Move {source} to {target}")
     os.rename(source, target)
